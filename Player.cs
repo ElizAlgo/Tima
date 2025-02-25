bafa590bd92ce254b5be9791322265a6cbea9d73
ElizAlgo <elizavetaalgoritmika24@gmail.com>
2025-02-25 17:01:12 +0000

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,19 @@
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+
+    private int health = 10;
+    public void Takedamage (int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока" + health);
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+     }
+
+
+}
