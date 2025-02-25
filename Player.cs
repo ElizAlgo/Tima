ebb37121a940df8356010b5409f29d6f50cea64c
ElizAlgo <elizavetaalgoritmika24@gmail.com>
2025-02-25 17:03:08 +0000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,3 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
     public GameObject fireballPrefab;
     public Transform attackPoint;
 
